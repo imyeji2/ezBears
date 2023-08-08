21e17bb5ac55eb50b5c1760d944667dc23002349
kiki4 <kiki4@DESKTOP-OTVQJO8>
2023-08-08 18:42:09 +0900

    코밋

---
+++ ezBears/src/main/java/com/ez/ezBears/staff/model/StaffDAO.java
@@ -0,0 +1,8 @@
+package com.ez.ezBears.staff.model;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface StaffDAO {
+	
+}
---
+++ ezBears/src/main/java/com/ez/ezBears/staff/model/StaffService.java
@@ -0,0 +1,8 @@
+package com.ez.ezBears.staff.model;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface StaffService {
+
+}
