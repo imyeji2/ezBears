da2dbe3c277ffd5ee998c03d9e270c2125d45f79
GunwooRoh <124752745+GunwooRoh@users.noreply.github.com>
2023-08-10 16:29:42 +0900

    Update StaffDAO.java

--- ezBears/src/main/java/com/ez/ezBears/staff/model/StaffDAO.java
+++ ezBears/src/main/java/com/ez/ezBears/staff/model/StaffDAO.java
@@ -4,10 +4,6 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface StaffDAO {
-<<<<<<< HEAD
 	int selectCheckId(String STAFF_ID);
 	String selectPwd(String STAFF_ID);
-=======
-	
->>>>>>> branch 'main' of https://github.com/GunwooRoh/FinalProject.git
 }
