d4a2ff33ab32012059f58bb020f71955e5a9a636
GunwooRoh <124752745+GunwooRoh@users.noreply.github.com>
2023-08-10 16:29:18 +0900

    Update MyBoardListVO.java

--- ezBears/src/main/java/com/ez/ezBears/myBoard/model/MyBoardListVO.java
+++ ezBears/src/main/java/com/ez/ezBears/myBoard/model/MyBoardListVO.java
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 package com.ez.ezBears.myBoard.model;
 
 import lombok.Data;
@@ -11,30 +9,3 @@ public class MyBoardListVO {
 
 
 }
-=======
-package com.ez.ezBears.myBoard.model;
-
-import lombok.Data;
-
-@Data
-public class MyBoardListVO {
-	private String mBoardName;
-	private int mBoardNo;
-
-
-}
->>>>>>> branch 'main' of https://github.com/GunwooRoh/FinalProject.git
-=======
-package com.ez.ezBears.myBoard.model;
-
-import lombok.Data;
-
-@Data
-public class MyBoardListVO {
-	private String mBoardName;
-	private int mBoardNo;
-
-
-}
-
->>>>>>> branch 'main' of https://github.com/GunwooRoh/FinalProject.git
