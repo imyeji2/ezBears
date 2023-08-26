01886dc73624589f14e00f12b87bd88721e1c1f1
kiki4 <kiki4@DESKTOP-OTVQJO8>
2023-08-26 16:27:43 +0900

    2023-08-26 로그인 디비 들어가긴 함

--- ezBears/src/main/java/com/ez/ezBears/myPage/controller/MyPageController.java
+++ ezBears/src/main/java/com/ez/ezBears/myPage/controller/MyPageController.java
@@ -1,5 +1,6 @@
 package com.ez.ezBears.myPage.controller;
 
+import java.math.BigDecimal;import java.sql.SQLException;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.ez.ezBears.attendance.model.AttendanceService;
 import com.ez.ezBears.attendance.model.AttendanceVO;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -39,8 +42,6 @@ public class MyPageController {
 		logger.info("개인근태조회 페이지 처리 결과, list.size={}", list.size());
 		
 		model.addAttribute("list", list);
-	
-		
 		
 		return "/mypage/attendanceCheck";
 		
@@ -48,7 +49,72 @@ public class MyPageController {
 	}
 	
 	@RequestMapping("/attendanceInOut")
-	public String attendanceInOut() {
+	public String attendanceInOut(HttpSession session, HttpServletRequest request, @RequestParam String date,
+			Model model) {
+		logger.info("session 확인 , session={}", session);
+		BigDecimal bigDecimalMemNo = (BigDecimal) session.getAttribute("memNo");
+		
+		// BigDecimal 값을 int로 변환
+		int memNo = bigDecimalMemNo.intValue();
+		logger.info("최근 출퇴근 조회, 파라미터 memNo={}", memNo);
+		
+		AttendanceVO attendanceVo = attendanceService.selectRecentAttendance(memNo);
+		logger.info("attendanceVo={}", attendanceVo);
+		
+		//attandaceVo 가 null 일 경우 (즉, 첫출근일 경우 처리)
+		if(attendanceVo == null) {
+			int cnt = attendanceService.insertAttendance(memNo);
+			logger.info("첫출근 처리 결과, cnt={}", cnt);
+		}else {
+			//출근시간 불러오기
+			String inTime = attendanceVo.getInTime();
+			logger.info("intime={}",inTime);
+			
+			//가장 최근의 출근날짜와 오늘의 날짜를 비교하기 위한 세팅
+			String subInTime = inTime.substring(0, 10);
+			logger.info("subInTime={}", subInTime);
+			logger.info("date= {}", date);
+			
+			//최근의 출근날짜가 오늘 날짜가 아닐 경우(출근 처리)
+			if(!subInTime.equals(date)) {
+				int cnt = attendanceService.insertAttendance(memNo);
+				logger.info("출근 처리 결과, cnt={}", cnt);
+			//최근의 출근날짜가 오늘 날짜일 경우
+			}else {
+				//1) 퇴근날짜가 없는 경우(퇴근 처리)
+				if(attendanceVo.getOutTime() == null || attendanceVo.getOutTime().isEmpty() ) {
+					int attendanceNo = attendanceService.selectAttendanceNo(memNo);
+					logger.info("attendanceNo={}",attendanceNo);
+					
+					int cnt = attendanceService.goHome(attendanceNo);
+					logger.info("퇴근 처리 결과, cnt={}", cnt);
+					
+				//2) 퇴근 날짜가 있는 경우(이미 출근처리 되었다고 표시)
+				}else {
+					String message= "이미 퇴근처리가 되었습니다.";
+					String url = "/";
+					
+					model.addAttribute("msg", message);
+					model.addAttribute("url", url);
+					
+					return "/common/message";
+				}
+				
+				
+			}
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 		return "/";
