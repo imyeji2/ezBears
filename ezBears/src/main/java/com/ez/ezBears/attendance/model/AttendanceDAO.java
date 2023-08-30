89807e3dbb8df5c74f57d266e5c2e384e85d21b4
kiki4 <kiki4@DESKTOP-OTVQJO8>
2023-08-30 16:06:12 +0900

    my page 에서 근태관리는 끝

--- ezBears/src/main/java/com/ez/ezBears/attendance/model/AttendanceDAO.java
+++ ezBears/src/main/java/com/ez/ezBears/attendance/model/AttendanceDAO.java
@@ -1,8 +1,10 @@
 package com.ez.ezBears.attendance.model;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface AttendanceDAO {
@@ -13,4 +15,8 @@ public interface AttendanceDAO {
 	int comeCompanyLate(int attendanceNo);
 	int comeCompanyGood(int attendanceNo);
 	int goHome(AttendanceVO attendanceVo);
+	List<Map<String, Object>> selectAllThisMonth(@Param ("memNo") int memNo, @Param("date") String date );
+	int countComeLate(@Param ("memNo") int memNo, @Param("date") String date );
+	int countComeGood(@Param ("memNo") int memNo, @Param("date") String date );
+	int countGoEarly(@Param ("memNo") int memNo, @Param("date") String date );
 }
