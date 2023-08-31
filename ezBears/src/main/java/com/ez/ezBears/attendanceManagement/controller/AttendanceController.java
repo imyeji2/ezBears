f190e36dabf165f97b6662e4766fb1fd57feefd7
kiki4 <kiki4@DESKTOP-OTVQJO8>
2023-08-31 15:34:29 +0900

    관리자 출퇴근 페이징처리까지 끝 이제 검색만 하면 출퇴근 완전 끝

--- ezBears/src/main/java/com/ez/ezBears/attendanceManagement/controller/AttendanceController.java
+++ ezBears/src/main/java/com/ez/ezBears/attendanceManagement/controller/AttendanceController.java
@@ -48,9 +48,40 @@ public class AttendanceController {
 		
 		List<DeptVO> deptList = deptService.selectDeptList();
 		model.addAttribute("deptList", deptList);
+		logger.info("여기까지는 되나?");
 		
-		List<Map<String, Object>> attendanceList = attendanceService.selectAllView();
+		//여기서 하게되면 searchVo가 아직 null 이라서 list 자체도 null 이 나오게 된다
+		/*
+		List<Map<String, Object>> attendanceList = attendanceService.selectAllView(searchVo);
 		logger.info("출/퇴근 검색 화면에 view 띄우기, attendanceList.size={}", attendanceList.size());
+		*/
+		
+		//-----------------------------여기부터는 페이징처리와 검색 처리를 위한 코드들--------------------------------------------
+		//pagination 객체 생성해서 없는 변수들 선언해준다
+		PaginationInfo pagination = new PaginationInfo();
+		pagination.setBlockSize(ConstUtil.BLOCK_SIZE);
+		pagination.setCurrentPage(searchVo.getCurrentPage());
+		pagination.setRecordCountPerPage(ConstUtil.RECORD_COUNT);
+		
+		//pagination 객체 이용해서 searchVo에 필요한 변수 마저 선언해주기
+		searchVo.setFirstRecordIndex(pagination.getFirstRecordIndex());
+		searchVo.setRecordCountPerPage(ConstUtil.RECORD_COUNT);
+		logger.info("설정 후 searchVo={}", searchVo);
+		
+		int totalRecord = attendanceService.countAllView();
+		pagination.setTotalRecord(totalRecord);
+		
+		model.addAttribute("pagination", pagination);
+		
+		List<Map<String, Object>> attendanceList = attendanceService.selectAllView(searchVo);
+		logger.info("출/퇴근 검색 화면에 view 띄우기, attendanceList.size={}", attendanceList.size());
+		
+		
+		
+		
+		
+		
+		
 		
 		//------------------------------이제부터 근무시간을 계산하기 위한 코드들---------------------------------------
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM");
@@ -63,44 +94,27 @@ public class AttendanceController {
 			logger.info("timestamp2={}", timestamp2);
 			
 			double hourGap = 0;  // 기본적으로 시간 차이를 0으로 설정
-
-		    if(timestamp2 != null) {  // date2가 비어있지 않은 경우에만 계산
-		    	String date1 = timestamp1.toString(); // Timestamp를 String으로 변환
-		    	String date2 = timestamp2.toString(); // Timestamp를 String으로 변환
-		    	
-		        Date format1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(date1);
-		        Date format2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(date2);
-
-		        // 시간의 차이 구하기
-		        hourGap = Math.ceil(((format2.getTime() - format1.getTime()) / 3600000d)*10)/10;
-		    }
-		    
-		    logger.info("hourGap={}", hourGap);
-		    
-		    map.put("hourGap", Double.toString(hourGap));
-		    logger.info("map={}", map);
+			
+			if(timestamp2 != null) {  // date2가 비어있지 않은 경우에만 계산
+				String date1 = timestamp1.toString(); // Timestamp를 String으로 변환
+				String date2 = timestamp2.toString(); // Timestamp를 String으로 변환
+				
+				Date format1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(date1);
+				Date format2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(date2);
+				
+				// 시간의 차이 구하기
+				hourGap = Math.ceil(((format2.getTime() - format1.getTime()) / 3600000d)*10)/10;
+			}
+			
+			logger.info("hourGap={}", hourGap);
+			
+			map.put("hourGap", Double.toString(hourGap));
+			logger.info("map={}", map);
 		}
 		
 		logger.info("출/퇴근 상세보기 처리 후, attendanceList.size={}", attendanceList.size());
 		
 		model.addAttribute("attendanceList", attendanceList);
-		
-		//-----------------------------여기부터는 페이징처리와 검색 처리를 위한 코드들--------------------------------------------
-		//pagination 객체 생성해서 없는 변수들 선언해준다
-		PaginationInfo pagination = new PaginationInfo();
-		pagination.setBlockSize(ConstUtil.BLOCK_SIZE);
-		pagination.setCurrentPage(searchVo.getCurrentPage());
-		pagination.setRecordCountPerPage(ConstUtil.RECORD_COUNT);
-		logger.info("pagination={}", pagination);
-		
-		//pagination 객체 이용해서 searchVo에 필요한 변수 마저 선언해주기
-		searchVo.setFirstRecordIndex(pagination.getFirstRecordIndex());
-		searchVo.setRecordCountPerPage(ConstUtil.RECORD_COUNT);
-		logger.info("설정 후 searchVo={}", searchVo);
-		
-		int totalRecord = attendanceService.countAllView();
-		
-		model.addAttribute("pagination", pagination);
 		
 		return "/attendanceManagement/attendanceSearch";
 		
